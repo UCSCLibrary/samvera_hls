b6fd14ff8bb7511b887d2c432ecf0b643e46e749
Ned Henry <ethenry@ucsc.edu>
2017-01-26 14:21:35 -0800

    Allow for public streaming

--- lib/generators/hydra_hls/install/templates/controllers/file_sets_controller.rb
+++ lib/generators/hydra_hls/install/templates/controllers/file_sets_controller.rb
@@ -1,4 +1,6 @@
 class FileSetsController < CurationConcerns::FileSetsController
+  skip_before_action :authenticate_user!, :only => [:show,:citation, :stats,  :embed, :master, :variant]
+  skip_authorize_resource :only => [:show,:citation, :stats,  :embed, :master, :variant]
   include HydraHls::FileSetsControllerBehavior  
 end
 
