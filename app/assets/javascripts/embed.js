cc1d0cd78f58b5ba12ff33a462e9f42f178153c7
Ned Henry <ned.henry@gmail.com>
2017-03-09 15:07:27 -0800

    removed redundant jquery include

--- app/assets/javascripts/embed.js
+++ app/assets/javascripts/embed.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
 //= require mediaelement-and-player
 //= require mep-feature-playlist
 //= require hls_initialize
