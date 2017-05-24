941c82a277fcaf6b9fbddf28410e9362de5f9240
Ned Henry <ethenry@ucsc.edu>
2017-05-23 18:09:15 -0700

    added message warning of playback problem on Chrome with older OSX versions

--- app/assets/javascripts/include_player.js
+++ app/assets/javascripts/include_player.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require browser_notification
 //= require mediaelement-and-player
 //= require mep-feature-playlist
 //= require hls_initialize
