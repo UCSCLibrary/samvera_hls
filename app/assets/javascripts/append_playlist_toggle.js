3580e5fcf5536e456d01bab64779101f659c0c2c
NedHenry <ethenry@ucsc.edu>
2019-05-29 09:07:16 -0700

    remove intrusive debugging code (that should not have been there)

--- app/assets/javascripts/append_playlist_toggle.js
+++ app/assets/javascripts/append_playlist_toggle.js
@@ -6,13 +6,3 @@ $( window ).load(function(){
 })
 
 window.onunload = function(){};
-
-$(window).bind("pageshow", function(event) {
-  alert('pageshow event firing')
-  if (event.originalEvent.persisted) {
-    alert('reloading the damn thing!')
-    window.location.reload(); 
-  } else {
-    alert('NOT reloading tsdhe damn thing!')
-  }
-});
